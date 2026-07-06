7c0dbeb9db7e3d12cee790dad827c998c8c5c603
adrianSalazar050 <adrian.salazar.gonzalez05@gmail.com>
2026-07-06 10:14:05 -0600

    Update PIN_MAP.h

--- PIN_MAP.h
+++ PIN_MAP.h
@@ -1,14 +1,12 @@
 #ifndef PIN_MAP_h
 #define PIN_MAP_h
 
-#define C1            18               // RAMPS: Z-MIN para señal A encoder
-#define C2            19               // RAMPS: Z-MAX para señal B encoder 
-
-#define pinFan         8               // RAMPS: Pin D8 para PWM
-
-#define pinMotor       9               // RAMPS: Pin D9 para control PWM
-
-#define pinHotend     10               // RAMPS: Pin D10 para control PWM      
-#define termPin       13               // RAMPS: Pin T0 para termistor (A13 del Arduino)
+// PIN MAP (RAMPS)
+#define C1            18               // Z-MIN: encoder signal A
+#define C2            19               // Z-MAX: encoder signal B
+#define pinFan         8               // D8:  fan PWM
+#define pinMotor       9               // D9:  DC motor PWM
+#define pinHotend     10               // D10: hotend PWM
+#define termPin       13               // T0:  thermistor (Arduino A13)
 
 #endif
